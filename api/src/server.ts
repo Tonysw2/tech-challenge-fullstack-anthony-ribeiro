2b3d0304922f428baf456184de75cb7d8bf3dd8a
Tonysw2 <anthonyrmataa@gmail.com>
2026-03-03 03:14:18 -0300

    chore(be): setup biome, husky and lint-staged

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,13 +1,13 @@
-import { app } from "./app/app";
+import { app } from './app/app'
 
 const start = async () => {
   try {
-    await app.listen({ port: 3333 });
-    console.log("Server running on port 3333 🔥");
+    await app.listen({ port: 3333 })
+    console.log('Server running on port 3333 🔥')
   } catch (err) {
-    app.log.error(err);
-    process.exit(1);
+    app.log.error(err)
+    process.exit(1)
   }
-};
+}
 
-start();
+start()
